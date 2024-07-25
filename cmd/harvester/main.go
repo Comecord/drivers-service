4a2a11650c295f49e36c1008693a46a65e413d85
Cold927 <colddevelop@gmail.com>
2024-07-25 14:20:46 +0300

    Проверка header при присоединении к вебсокет серверу через email с mongo

--- cmd/harvester/main.go
+++ cmd/harvester/main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"context"
+	"crm-glonass/config"
+	"crm-glonass/data/mongox"
 	"crm-glonass/harvester"
+	"crm-glonass/pkg/logging"
 	"fmt"
 	"github.com/gorilla/websocket"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 	"net/http"
+	"sync"
 )
 
 type Message struct {
 	UserID string `json:"userID"`
 	Type   string `json:"type"`
 }
+
+var (
+	conf    = config.GetConfig()
+	logger  = logging.NewLogger(config.GetConfig())
+	clients = make(map[*Client]bool) // Порог блокировки
+)
+var mu sync.RWMutex
 
 // Функция для обработки сообщений "vehicles"
 func VehicleService(msg Message) map[string]string {
@@ -42,8 +56,6 @@ type Client struct {
 	Conn *websocket.Conn
 }
 
-var clients = make(map[*Client]bool)
-
 func main() {
 	http.HandleFunc("/ws", handleConnections)
 
@@ -54,11 +66,12 @@ func main() {
 }
 
 func handleConnections(w http.ResponseWriter, r *http.Request) {
-	//authHeader := r.Header.Get("Authorization")
-	//if authHeader == "" {
-	//	http.Error(w, "Unauthorized", http.StatusUnauthorized)
-	//	return
-	//}
+	ctx := context.TODO()
+	db, err := mongox.Connection(conf, ctx, logger)
+	if err != nil {
+		fmt.Println("Error getting MongoDB client:", err)
+		return
+	}
 
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
@@ -67,16 +80,20 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 	}
 	defer conn.Close()
 
-	token := r.Header.Get("token")
-	if token == "" {
-		fmt.Println("Токен отсутствует, отключение клиента...")
-		http.Error(w, "Forbidden", http.StatusForbidden) // Отправляем ответ и закрываем соединение
+	var resultEmail struct {
+		Email string `bson:"email"`
+	}
+
+	email := r.Header.Get("Auth-Email")
+
+	err = db.Collection("members").FindOne(ctx, bson.M{"email": email}).Decode(&resultEmail)
+	if err == mongo.ErrNoDocuments {
+		conn.WriteMessage(websocket.TextMessage, []byte("Email не найден"))
+		return
+	} else if err != nil {
+		conn.WriteMessage(websocket.TextMessage, []byte("Ошибка базы данных"))
 		return
 	}
-	client := &Client{Conn: conn}
-	clients[client] = true
-
-	fmt.Println("Client connected:", conn.RemoteAddr())
 
 	// Определение маршрутов
 	routes := ServerRoutes{
